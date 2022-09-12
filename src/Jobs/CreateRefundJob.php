3274bba5f2f466c49bf5c7edab42ec344440a5de
Tyno <mrtyno@outlook.com>
2022-09-12 16:17:11 +0200

    Refund gateway fix

--- src/Jobs/CreateRefundJob.php
+++ src/Jobs/CreateRefundJob.php
@@ -38,13 +38,15 @@ class CreateRefundJob implements ShouldQueue
             Log::error('Could not create refund for' . $this->refund->getTransactionId() . ' domain did not a have store in database', ['domain' => $this->refund->getDomain(), 'event' => 'refund_job_no_shop',]);
         }
 
+        $gateway = $multiSafepay->getOrderGateway($shop->multisafepay_api_key,$this->refund->getTransactionId());
+
         $refund = new MSPRefund\Refund($this->refund->getTransactionId(), $this->refund->getRefundId(), $this->refund->getAmount(), $this->refund->getCurrency(),);
         $hasSucceeded = $multiSafepay->createRefund($shop->multisafepay_api_key, $refund);
         if ($hasSucceeded) {
-            ShopifyGraphQL::resolveRefund($shop, $this->refund->getRefundGid(), '');
+            ShopifyGraphQL::resolveRefund($shop, $this->refund->getRefundGid(), $gateway);
             Log::info('Resolved refund for transaction ' . $this->refund->getTransactionId(), ['event' => 'refund_job_resolve', 'refund' => $this->refund->getRefundGid(), 'Refund: ' . $this->refund->getTransactionId()]);
         } else {
-            ShopifyGraphQL::rejectRefund($shop, $this->refund->getRefundGid(), 'refund failed');
+            ShopifyGraphQL::rejectRefund($shop, $this->refund->getRefundGid(), 'refund failed',$gateway);
             Log::error('Rejected refund for transaction ' . $this->refund->getTransactionId() . ' could not create refund', ['message' => 'refund failed', 'event' => 'refund_job_rejected', 'refund' => $this->refund->getRefundGid(),]);
         }
     }
