90575154c355422bc1a634bfbab16f4017b61a88
tim.remeeus <tim.remeeus@multisafepay.com>
2022-09-19 11:15:07 +0200

    VerifyShopifyRequest edit

--- src/Middleware/VerifyShopifyRequest.php
+++ src/Middleware/VerifyShopifyRequest.php
@@ -63,7 +63,7 @@ class VerifyShopifyRequest
 
         //Create data string for the remaining url parameters
         $dataString = self::buildQueryString($data);
-        $clientSecret = 'shopify.' . strtolower($request->get('gateway')) . '.secret';
+        $clientSecret = config('shopify.' . $request->get('gateway') . '.key');
 
         Log::info('GATEWAY',[$clientSecret,config($clientSecret)]);
 
