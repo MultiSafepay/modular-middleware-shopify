988f967f8a43b81ce5bd849ab9c7650c9fbb0d3c
tim.remeeus <tim.remeeus@multisafepay.com>
2022-09-23 14:55:04 +0200

    add ideal to config

--- config/shopify.php
+++ config/shopify.php
@@ -9,5 +9,9 @@ return [
     'creditcard' => [
         'secret' => env('SHOPIFY_CREDITCARD_SECRET', ''),
         'key' => env('SHOPIFY_CREDITCARD_KEY', '')
+    ],
+    'ideal' => [
+        'secret' => env('SHOPIFY_IDEAL_SECRET', ''),
+        'key' => env('SHOPIFY_IDEAL_KEY', '')
     ]
 ];
